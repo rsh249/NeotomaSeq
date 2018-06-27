ef51b2fca6e07f8609376ea1c289c817e71c2c28
Rob Harbert <rsh249@cornell.edu>
2018-06-27 00:28:35 -0400

    FALCON-Meta datavis
    Added scripts for the vidualization of FALCON metagenomic classification.

---
+++ FALCONVIS/falconvis.R
@@ -0,0 +1,69 @@
+##Visualize FALCON results from top hits file
+library(ggplot2)
+library(taxonomizr)
+
+setwd('~/FALCONVIS')
+top = read.delim("top.csv", sep="\t", header=FALSE)
+#parse accession numbers, ref length, and percent similarity to separate table
+
+r = 1;
+accID= vector();
+for (i in 1:nrow(top)){
+#for (i in 1:1000){
+ # print(top[i,1:3])
+  split = strsplit(as.character(top[i,4]), "_")
+  accID[[r]]= (split[[1]][1])
+  r = r+1;
+  
+
+}
+
+taxaId<-accessionToTaxa(accID,"accessionTaxa.sql")
+taxaNodes<-read.nodes('nodes.dmp') #takes time?
+taxaNames<-read.names('names.dmp')
+
+taxtable= getTaxonomy(taxaId,taxaNodes,taxaNames)
+
+taxtable = cbind(taxtable, top)
+
+
+phyla = stats::na.omit(plyr::count(taxtable$phylum))
+png(filename = "FALCONphyla.png", height = 7, width = 11, units = "in", res = 600)
+phyla <- phyla[order(-phyla$freq),]
+phyla$x <- factor(phyla$x, levels = phyla$x[order(-phyla$freq)])
+ggplot(data = phyla[1:50,], aes_string(x = colnames(phyla)[1], y = "freq")) + geom_col() + labs(x = "Phylum") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+dev.off()
+
+
+family = stats::na.omit(plyr::count(taxtable$family))
+png(filename = "FALCONfamily.png", height = 7, width = 11, units = "in", res = 600)
+family <- family[order(-family$freq),]
+family$x <- factor(family$x, levels = family$x[order(-family$freq)])
+ggplot(data = family[1:50,], aes_string(x = colnames(family)[1], y = "freq")) + geom_col() +  labs(x = "Family") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+dev.off()
+
+genus = stats::na.omit(plyr::count(taxtable$genus))
+png(filename = "FALCONgenus.png", height = 7, width = 11, units = "in", res = 600)
+genus <- genus[order(-genus$freq),]
+genus$x <- factor(genus$x, levels = genus$x[order(-genus$freq)])
+ggplot(data = genus[1:50,], aes_string(x = colnames(genus)[1], y = "freq")) + geom_col() + labs(x = "Genus") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+dev.off()
+
+#plants only
+plants = subset(taxtable, taxtable$phylum=='Streptophyta')
+pfamily = stats::na.omit(plyr::count(plants$family))
+png(filename = "FALCONpfamily.png", height = 7, width = 11, units = "in", res = 600)
+pfamily <- pfamily[order(-pfamily$freq),]
+pfamily$x <- factor(pfamily$x, levels = pfamily$x[order(-pfamily$freq)])
+ggplot(data = pfamily[1:50,], aes_string(x = colnames(pfamily)[1], y = "freq")) + geom_col() + labs(x = "Plant genera") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+dev.off()
+
+pgenus = stats::na.omit(plyr::count(plants$genus))
+png(filename = "FALCONpgenus.png", height = 7, width = 11, units = "in", res = 600)
+pgenus <- pgenus[order(-pgenus$freq),]
+pgenus$x <- factor(pgenus$x, levels = pgenus$x[order(-pgenus$freq)])
+ggplot(data = pgenus[1:50,], aes_string(x = colnames(pgenus)[1], y = "freq")) + geom_col() + labs(x = "Plant genera") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+dev.off()
+
+
+
---
+++ FALCONVIS/taxonomizr_build.R
@@ -0,0 +1,9 @@
+##Build taxonomizr database
+library(taxonomizr)
+libdir='taxonomizr'
+dir.create(libdir)
+getNamesAndNodes()
+getAccession2taxid()
+read.accession2taxid(list.files('.','accession2taxid$'),'accessionTaxa.sql')
+print(paste('taxonomizr database built and located at', getwd(), sep=' '))
+      
