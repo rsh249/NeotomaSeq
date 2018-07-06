9c7ab24ee2d1543f5ff213d75a614289432562b6
Rob Harbert <rsh249@cornell.edu>
2018-07-06 14:23:06 -0400

    taxonomizr build fix

--- FALCONVIS/taxonomizr_build.R
+++ FALCONVIS/taxonomizr_build.R
@@ -7,6 +7,6 @@ dir.create(libdir)
 setwd(libdir)
 getNamesAndNodes()
 getAccession2taxid()
-read.accession2taxid(list.files('.','accession2taxid$'),'accessionTaxa.sql')
+read.accession2taxid(list.files('.','accession2taxid'),'accessionTaxa.sql')
 print(paste('taxonomizr database built and located at', getwd(), sep=' '))
       
