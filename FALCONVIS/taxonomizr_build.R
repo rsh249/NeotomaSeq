8c3ac899248a693a84c1dd6f5c071493ab02c7ac
Rob Harbert <rsh249@cornell.edu>
2018-07-24 14:36:05 -0400

    taxonomizr build update

--- FALCONVIS/taxonomizr_build.R
+++ FALCONVIS/taxonomizr_build.R
@@ -6,7 +6,9 @@ libdir=targ
 dir.create(libdir)
 setwd(libdir)
 getNamesAndNodes()
+getAccession2taxid(types=c('nucl_gb'))
 getAccession2taxid()
+system("gunzip *.gz")
 read.accession2taxid(list.files('.','accession2taxid'),'accessionTaxa.sql')
 print(paste('taxonomizr database built and located at', getwd(), sep=' '))
       
